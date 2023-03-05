cc5745f6a8d4ca3c2c2cb7670ec418c279c8d1c0
tynkovdev <124764376+tynkovdev@users.noreply.github.com>
2023-03-05 13:27:09 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require(`express`);
+const hbs = require(`hbs`)
+const app = express();
+
+const port = 8080;
+
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+});
+
+app.set(`views`, `views`);
+app.set(`view engine`, `hbs`);
+
+app.get(`/`, (req, res) => {
+    red.render(`index`);
+});
